baae776a667b7cf30e7dbab8dd75a2d568b145de
jikuohong <34758287+jikuohong@users.noreply.github.com>
2020-02-03 22:01:59 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,8 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+git clone https://github.com/hong0980/luci-app-passwall.git
+git clone https://github.com/jikuohong/luci-app-serverchan.git
+
+./scripts/feeds update -a
+./scripts/feeds install -a
